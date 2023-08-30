f40ae38fd41c36c50ab4db15eed6d3a530fa1017
nucgeo <tsemanoa@cryptolab.net>
2023-08-30 16:42:35 -0300

    db conectado

--- migrations/versions/f04a2af5295e_create_users_table.py
+++ migrations/versions/f04a2af5295e_create_users_table.py
@@ -7,9 +7,8 @@ Create Date: 2023-08-30 14:48:40.763251
 """
 from typing import Sequence, Union
 
-from alembic import op
 import sqlalchemy as sa
-
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = 'f04a2af5295e'
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,3 +1,6 @@
+from fast_zero.schemas import UserPublic
+
+
 def test_root_deve_retornar_200_e_ola_mundo(client):
     response = client.get('/')
 
@@ -7,7 +10,7 @@ def test_root_deve_retornar_200_e_ola_mundo(client):
 
 def test_create_user(client):
     response = client.post(
-        '/users/',
+        '/users',
         json={
             'username': 'alice',
             'email': 'alice@example.com',
@@ -23,20 +26,12 @@ def test_create_user(client):
 
 
 def test_read_users(client):
-    response = client.get('/users/')
+    response = client.get('/users')
     assert response.status_code == 200
-    assert response.json() == {
-        'users': [
-            {
-                'username': 'alice',
-                'email': 'alice@example.com',
-                'id': 1,
-            }
-        ]
-    }
+    assert response.json() == {'users': []}
 
 
-def test_update_user(client):
+def test_update_user(client, user):
     response = client.put(
         '/users/1',
         json={
@@ -53,8 +48,13 @@ def test_update_user(client):
     }
 
 
-def test_delete_user(client):
+def test_delete_user(client, user):
     response = client.delete('/users/1')
-
     assert response.status_code == 200
     assert response.json() == {'detail': 'User deleted'}
+
+
+def test_read_users_with_users(client, user):
+    user_schema = UserPublic.model_validate(user).model_dump()
+    response = client.get('/users/')
+    assert response.json() == {'users': [user_schema]}
